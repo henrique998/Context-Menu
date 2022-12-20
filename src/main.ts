02e43cbe3f507cda24b135a416e74f10e32827a4
henrique monteiro <henriquemonteiro037@gmail.com>
2022-12-20 03:24:13 +0000

    app finished

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,19 @@
 import './styles/global.scss'
 import './styles/main.scss'
+
+const menuContainer = document.querySelector<HTMLDivElement>('[data-element="menu-container"]')
+
+document.addEventListener('contextmenu', e => {
+    e.preventDefault()
+
+    let xCoordinate = e.offsetX
+    let yCoordinate = e.offsetY
+
+    menuContainer!.style.left = `${xCoordinate}px`
+    menuContainer!.style.top = `${yCoordinate}px`
+    menuContainer!.style.visibility = "visible"
+})
+
+document.addEventListener('click', () => {
+    menuContainer!.style.visibility = "hidden"
+})
